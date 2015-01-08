1234ef9bbe9e750fe1d72178b3088f0400f4751c
clemtoy <cmich@hotmail.fr>
2015-01-08 17:15:03 +0100

    extreme

--- src/orientation.lua
+++ src/orientation.lua
@@ -1,6 +1,6 @@
 orientation = dark.pipeline()
 orientation:pattern([[
 	[&orientation
-		( /capitaliste/ | /^[Sc]ocialiste/ | /^[Cc]ommuniste/ | /libéral/ | ( /^extrême$/? -? ( /^gauch/ | /^droite$/ ) )  | /^centre$/ | /^anarchiste/ | /^révolution/ )
+		( /capitaliste/ | /^[Sc]ocialiste/ | /^[Cc]ommuniste/ | /libéral/ | ( /^extrême$/? /^-$/? ( /^gauch/ | /^droite$/ ) )  | /^centre$/ | /^anarchiste/ | /^révolution/ )
 	]
 ]])
