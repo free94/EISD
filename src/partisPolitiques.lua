2b32d358175063011374f9bb080259fec03308e4
clemtoy <cmich@hotmail.fr>
2015-01-08 16:16:12 +0100

    partis improved

--- src/partisPolitiques.lua
+++ src/partisPolitiques.lua
@@ -1,22 +1,22 @@
-partis = dark.pipeline()
-partis:pattern([[
+parti = dark.pipeline()
+parti:pattern([[
 	[&role
 		( /^[Cc]andidat/ | /^[Aa]dhèr/ | /^[Pp]résid/ | ( /^[Ss]ecrétaire$/ /^nationale?$/? ) | /^[Dd]éputée?$/ )
 	]
 ]])
 
-partis:pattern([[
+parti:pattern([[
 	[&adhesion
 			( /^rejoin/ | /^cré[eaé]$/ | /^dirige/ )
 	]
 ]])
-partis:pattern([[
+parti:pattern([[
 	[&det
 		( /^d[eu]s?$/ | /^l[ea]?s?$/ | /^à$/ | au ) /^'$/?
 	]
 ]])
 
-partis:pattern([[
+parti:pattern([[
 	[&expression_parti
 		(
 			( &role | &adhesion )
