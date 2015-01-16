7dff4b0a9bb3e888c70fa8224ad93b5a1d5a290f
free94 <florianelmalek2@msn.com>
2015-01-16 10:59:29 +0100

    ajout bd.lua

---
+++ src/bd.lua
@@ -0,0 +1,57 @@
+ingredients = {
+	"jaune d'oeuf" = {
+		categorie = "viande",
+		substituts = {
+			"rhum",
+		},
+	},
+}
+
+recettes={
+	advocat = {
+		temps_preparation = {
+			valeur = 30,
+			unite = "minute",
+		},
+		temps_cuisson = {
+			valeur = 30,
+			unite = "minute",
+		},
+		ingredients = {
+			"jaune d'oeuf" = {				
+				quantite = 10,
+				unite = nil,
+			},
+			"sucre semoule" = {
+				quantite = 200,
+				unite = "g",
+			}
+		}
+		quantite = {
+			valeur = 12,
+			unite = "personnes",
+		},
+		etapes	= {
+			{
+				consigne = "pre chauffer four",
+				duree = {
+					valeur = ,
+					unite = ,
+				},
+			},
+		},
+		difficulte = "hardcore",
+		origine = "Perou",
+		popularite = 569874,
+		remarques = "cette recette a été créée par Monsieur Michard à EDC le 12 janvier 2015.",
+		avis = {		
+			"c'était IMMONDE",		
+		},
+		prix = {
+			"pas cher",
+		},
+		outils = {
+			"fouet",
+		},		
+	},	
+}
