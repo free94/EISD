334bd7a6a93adedf8f1ee9ed3c948ec81f5e2998
clemtoy <cmich@hotmail.fr>
2015-01-16 21:46:06 +0100

    structure de la recette

--- src/main.lua
+++ src/main.lua
@@ -1,24 +1,36 @@
 require('etape')
--- require('avis')
 require('quantite')
+require('quantite')
+require('structure')
 
 main = dark.pipeline()
 main:model("model/postag-fr")
 main:add(etape)
--- main:add(avis)
 main:add(quantite)
+main:add(structure)
 
 -- tags a afficher
 tags = {
-	etape    = 'red',
+	--[[etape    = 'red',
   quantite = 'green',
   valeur   = 'green',
-  unite    = 'green',
+  unite    = 'green',]]
+  temps_preparation = 'yellow',
+  temps_cuisson = 'yellow',
+  ingredients = 'yellow',
+  preparation = 'yellow',
+  manipulation = 'yellow',
+  extra = 'yellow'
 }
 
 -- affichage
+--[[
 for line in io.lines() do
   seq = main(line:gsub('%p', ' %1 '))
   print(seq:tostring(tags))
   -- seq:dump()
 end
+]]
+
+seq = main(io.read("*all"):gsub('%p', ' %1 '))
+print(seq:tostring(tags))
---
+++ src/structure.lua
@@ -0,0 +1,16 @@
+structure = dark.pipeline()
+
+--[[
+	Remarque : comme je ne sais pas matcher les retours à la ligne ni les EOF ni faire des exclusions avec dark,
+	je ne trouve pas comment faire détecter extra autrement qu'avec la solution suivante.
+	Pour la même raison, le tagging sera erroné dans le cas d'une concaténation de fichiers.
+]]
+
+structure:pattern([[
+		[&temps_preparation Temps de /^préparation*/ /^:$/ &NUM . ]
+		[&temps_cuisson Temps de cuisson /^:$/ &NUM . ]
+		[&ingredients /^Ingrédient/ /^%($/ pour &NUM . /^%)$/ /^:$/ .*? ]
+		[&preparation /^Préparation$/ de la recette /^:$/ .* ]
+	]])
+structure:pattern("[&extra Remarques /^:$/ .* ]")
+structure:pattern("&ingredients [&manipulation .*?] &extra")
