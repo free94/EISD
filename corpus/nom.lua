e2d6551a45d31545379a5eda4ebe97e314f920c5
Alexis Gauthiez <alexis.gauthiez@u-psud.fr>
2015-01-29 16:38:43 +0100

    nom.lua fix

--- corpus/nom.lua
+++ corpus/nom.lua
@@ -1,3 +1,4 @@
 local nom_sans_recette=string.gsub(arg[1],"recette_"," ")
 nom_sans_recette=string.gsub(nom_sans_recette,"_%d*%.txt"," ")
 local nom_final=string.gsub(nom_sans_recette,"-"," ")
+print(nom_final)
